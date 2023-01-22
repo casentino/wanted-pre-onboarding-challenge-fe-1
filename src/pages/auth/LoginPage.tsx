1f8fbab21fd9cb9dd98e214b78e19370630ef812
casentino <opterk@gmail.com>
2023-01-23 03:46:42 +0900

    로그인 스토어 제거

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import { Grid } from '@mui/material';
-import LoginStoreProvider from '../../components/auth/login/LoginStoreProvider';
 import LoginArea from '../../components/auth/LoginArea';
 
 function LoginPage() {
   return (
-    <LoginStoreProvider>
-      <Grid
-        container
-        direction="row"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Grid item>
-          <LoginArea />
-        </Grid>
+    <Grid
+      container
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Grid item>
+        <LoginArea />
       </Grid>
-    </LoginStoreProvider>
+    </Grid>
   );
 }
 export default LoginPage;
